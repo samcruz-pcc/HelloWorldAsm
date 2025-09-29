db2f710e0f7e6beca6e97da7c8467fbb5ef15e23
Samantha Cruz <scruz50@go.pasadena.edu>
2025-09-29 04:38:58 +0000

    nasm example

--- helloWorld.asm
+++ helloWorld.asm
@@ -1,18 +1,22 @@
 section .data
-msg: DB 'Hello World!', 10
-msgSize EQU $ - msg
-
-global start
+    num1   dd 5         ;define num1 as 5
+    num2   dd 3         ;define num2 as 3
+    result dd 0         ;define rersult as 0
 
 section .text
+    global _start
 
-    start:
-    mov rax, 0x2000004          ; function 4
-    mov rbx, 1          ; stdout
-    mov rcx, msg        ; msg
-    mov rdx, msgSize    ; size
+_start:
+    mov eax, [num1]     ; load num1 (5) into eax
+    add eax, [num2]     ; add num2 (3) to eax
+    mov [result], eax   ; store result (8) into memory
+
+    mov ebx, [result]   ; load result into ebx
+    sub ebx, 2          ; subtract 2 from result (6)
+    mov [result], ebx   ; store updated result
+
+    ; exit syscall
+    mov eax, 1
+    xor ebx, ebx
     int 0x80
-    mov rax, 1          ; function 1
-    mov rbx, 0          ; code
-    int 0x80
-    ret
+
